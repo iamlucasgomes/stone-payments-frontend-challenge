da91aec4e69826ec46703746f76df1b14c6df5f3
iamlucasgomes <lucas.devjs@gmail.com>
2023-05-19 21:35:33 -0300

    style: logo and dollar svg added

---
+++ src/mock/MockAPI.ts
@@ -0,0 +1,17 @@
+const RESPONSE = {
+	"USDBRL": {
+		"code": "USD",
+		"codein": "BRL",
+		"name": "Dólar Americano/Real Brasileiro",
+		"high": "5.0033",
+		"low": "4.944",
+		"varBid": "0.0328",
+		"pctChange": "0.66",
+		"bid": "4.9976",
+		"ask": "4.9983",
+		"timestamp": "1684529993",
+		"create_date": "2023-05-19 17:59:53"
+	}
+}
+
+export default RESPONSE;
