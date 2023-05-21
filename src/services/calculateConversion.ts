60da8b44717ab55cc840574a55e68345f14dc192
iamlucasgomes <lucas.devjs@gmail.com>
2023-05-21 15:42:57 -0300

    feat: calculateConversion function added

---
+++ src/services/calculateConversion.ts
@@ -0,0 +1,13 @@
+import cardConversion from "./cardConversion";
+import moneyConversion from "./moneyConversion";
+
+type ConversionStrategy = () => string;
+
+export default function calculateConversion(type: 'money' | 'card', dolar: number, tax: number, dolarValue: number): string {
+  const CONVERSION_STRATEGIES: Record<string, ConversionStrategy> = {
+    'money': () => moneyConversion(dolar, tax, dolarValue),
+    'card': () => cardConversion(dolar, tax, dolarValue)
+  };
+
+  return CONVERSION_STRATEGIES[type]();
+}
