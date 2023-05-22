a1de916714c67164da0ba540e6121448a2ede760
iamlucasgomes <lucas.devjs@gmail.com>
2023-05-21 23:53:52 -0300

    feat:api response data inserted into the application

--- src/services/calculateConversion.ts
+++ src/services/calculateConversion.ts
@@ -4,6 +4,7 @@ import moneyConversion from "./moneyConversion";
 type ConversionStrategy = () => string;
 
 export default function calculateConversion(type: string, dolar: number, tax: number, dolarValue: number): string {
+  console.log(tax)
   const CONVERSION_STRATEGIES: Record<string, ConversionStrategy> = {
     'money': () => moneyConversion(dolar, tax, dolarValue),
     'card': () => cardConversion(dolar, tax, dolarValue)
